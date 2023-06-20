3d2d882a3dda27d2c44be023844c17611d238f6c
Frank Alimimian <alimimianfrank@gmail.com>
2023-06-20 05:23:40 +0100

    AddPost request method to menu-items view

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -3,9 +3,17 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import CartSerializer,MenuItemSerializer,OrderItemSerializer,OrderSerializer,CategorySerializer
 from .models import Cart,MenuItem,Order,OrderItem,Category
+from rest_framework import status
 # Create your views here.
-@api_view()
+@api_view(['POST','GET'])
 def menu_items(request):
-  items = MenuItem.objects.select_related('category').all()
-  serialized_items = MenuItemSerializer(items, many=True)
-  return Response(serialized_items.data)
+  if request.method == 'GET':
+    items = MenuItem.objects.select_related('category').all()
+    serialized_items = MenuItemSerializer(items, many=True)
+    return Response(serialized_items.data)
+  
+  elif request.method == 'POST':
+    serialized_items = MenuItemSerializer(data = request.data)
+    serialized_items.is_valid(raise_exception=True)
+    serialized_items.save()
+    return Response(serialized_items.data,status.HTTP_201_CREATED)
