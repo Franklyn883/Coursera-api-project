77257b6002359111a4fe201a48377e5a5e2d899f
Frank Alimimian <alimimianfrank@gmail.com>
2023-06-20 03:47:50 +0100

    Add data validation

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -13,6 +13,9 @@ class MenuItemSerializer(serializers.ModelSerializer):
     model = MenuItem
     fields = ['id','title','price', 'featured','category']
     
+    def validate_price(self,value):
+      if(value < 0):
+        raise serializers.ValidationError('price should be more that zero')
     
     
 class CartSerializer(serializers.ModelSerializer):
@@ -21,12 +24,24 @@ class CartSerializer(serializers.ModelSerializer):
     fields = ['id','quantity','unit_price', 'price']
     depth = 1
     
+    def validate(self,attrs):
+      if(attrs['quantity']<0):
+        raise serializers.ValidationError("Quantity should be more than 0")
+      if(attrs['unit_price']<0):
+        raise serializers.ValidationError("price should be more than zero")
+      return super().validate(attrs)
+    
 class OrderSerializer(serializers.ModelSerializer):
+  
   model = Order
   fields = ['id', 'status','total','date']
   depth = 1
   
 class OrderItemSerializer(serializers.ModelSerializer):
+  unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+  quantity = serializers.IntegerField()
   model = OrderItem
   fields = ['id', 'quantity', 'unit_price','price']
   depth = 1
+  
+  
