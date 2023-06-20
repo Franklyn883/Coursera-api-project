29cf7a914dab0c7d769a08849545639bfd4d95a4
Frank Alimimian <alimimianfrank@gmail.com>
2023-06-20 07:49:36 +0100

    Update urls.py

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
   path('menu-items',views.menu_items),
   path('menu-items/<int:pk>',views.menu_item),
   path('login/', include('djoser.urls')),
+  path('users/', include('djoser.urls'))
  
 ]
